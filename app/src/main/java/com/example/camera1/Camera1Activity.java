7292a851caa83aed8914c3f5e039f93ecb1ac92a
lijunjun <gionchat@gmail.com>
2022-06-06 11:11:54 +0800

    1、合入Camera1 使用相关代码

--- app/src/main/java/com/example/camera1/Camera1Activity.java
+++ app/src/main/java/com/example/camera1/Camera1Activity.java
@@ -1,16 +1,119 @@
 package com.example.camera1;
 
+import android.app.AlertDialog;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.camerax.R;
 
-public class Camera1Activity extends AppCompatActivity {
+
+
+// ----------------------------------------------------------------------
+
+public class Camera1Activity extends AppCompatActivity implements Camera.PreviewCallback {
+    private Camera1Preview mPreview;
+    Camera mCamera;
+    int numberOfCameras;
+    int cameraCurrentlyLocked;
+
+    // The first rear facing camera
+    int defaultCameraId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera1);
+//
+//        // Hide the window title.
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Create a RelativeLayout container that will hold a SurfaceView,
+        // and set it as the content of our activity.
+        mPreview = findViewById(R.id.surface);
+
+        // Find the total number of cameras available
+        numberOfCameras = Camera.getNumberOfCameras();
+
+        // Find the ID of the default camera
+        CameraInfo cameraInfo = new CameraInfo();
+            for (int i = 0; i < numberOfCameras; i++) {
+                Camera.getCameraInfo(i, cameraInfo);
+                if (cameraInfo.facing == CameraInfo.CAMERA_FACING_BACK) {
+                    defaultCameraId = i;
+                }
+            }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Open the default i.e. the first rear facing camera.
+        mCamera = Camera.open();
+        mCamera.setPreviewCallback(this);
+        mCamera.setDisplayOrientation(90);
+        cameraCurrentlyLocked = defaultCameraId;
+        mPreview.setCamera(mCamera);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Because the Camera object is a shared resource, it's very
+        // important to release it when the activity is paused.
+        if (mCamera != null) {
+            mPreview.setCamera(null);
+            mCamera.setPreviewCallback(null);
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    private void switchCam() {
+        if (numberOfCameras == 1) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Device has only one camera!")
+                   .setNeutralButton("Close", null);
+            AlertDialog alert = builder.create();
+            alert.show();
+            return;
+        }
+
+        // OK, we have multiple cameras.
+        // Release this camera -> cameraCurrentlyLocked
+        if (mCamera != null) {
+            mCamera.stopPreview();
+            mPreview.setCamera(null);
+            mCamera.setPreviewCallback(null);
+            mCamera.release();
+            mCamera = null;
+        }
+
+        // Acquire the next camera and request Preview to reconfigure
+        // parameters.
+        mCamera = Camera
+                .open((cameraCurrentlyLocked + 1) % numberOfCameras);
+        cameraCurrentlyLocked = (cameraCurrentlyLocked + 1)
+                % numberOfCameras;
+        mPreview.switchCamera(mCamera);
+
+        // Start the preview
+        mCamera.startPreview();
+    }
+
+    @Override
+    public void onPreviewFrame(byte[] data, Camera camera) {
+        if(camera == null) return;
+        Camera.Size size = camera.getParameters().getPreviewSize(); //获取预览大小
+        final int w = size.width;
+        final int h = size.height;
     }
 }
+
+// ----------------------------------------------------------------------
